8913490f09b1cf3522cb5713e958663f76333d3e
KEVIN <38305127+kkao99@users.noreply.github.com>
2018-09-24 11:54:32 -0400

    Update kevin.php

--- kevin.php
+++ kevin.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>KEVIN KAO</title>
+  <title>Kevin Kao</title>
   <link rel="icon" href="img/icon.png">
   <link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" href="css/navigation.css">
