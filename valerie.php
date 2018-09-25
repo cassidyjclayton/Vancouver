0b01312b41e41cd876c62d363c1dc1330e947118
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-24 22:00:00 -0400

    Removed <span>sfrom Valerie (take 3)

--- valerie.php
+++ valerie.php
@@ -27,10 +27,10 @@
 			</figure>
 
 			<p>
-				<span>I</span>’m a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events.</p>
+				Im a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events.</p>
 
 			<p>
-				<span>I</span> was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys.</p>
+				I was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys.</p>
 
 		<h2 class="title">First Jobs</h2>
 			<figure class="img2">
@@ -39,10 +39,10 @@
 			</figure>
 
 			<p>
-				<span>M</span>y first job was at Mini Ivy Tutoring Center, where I tutored elementary and middle school students in math and English. In addition, I performed various office administrative duties, such as keeping track of attendance and picking up phone calls.</p>
+				My first job was at Mini Ivy Tutoring Center, where I tutored elementary and middle school students in math and English. In addition, I performed various office administrative duties, such as keeping track of attendance and picking up phone calls.</p>
 
 			<p>
-				<span>I</span>n the summer of 2015, I participated in Girls Who Code’s Summer Immersion Program, where I was introduced to computer science. I learned how to code in JavaScript, Python, and HTML. Besides gaining technical skills, I was able to connect with female engineers and entrepreneurs, and hear their stories about what it’s like to work in a tech company. The program also gave me the opportunity to visit different tech companies, such as Google and Microsoft. For the final project, I created a user-friendly online app using HTML and JavaScript to locate and post about volunteer services.</p>
+				In the summer of 2015, I participated in Girls Who Code’s Summer Immersion Program, where I was introduced to computer science. I learned how to code in JavaScript, Python, and HTML. Besides gaining technical skills, I was able to connect with female engineers and entrepreneurs, and hear their stories about what it’s like to work in a tech company. The program also gave me the opportunity to visit different tech companies, such as Google and Microsoft. For the final project, I created a user-friendly online app using HTML and JavaScript to locate and post about volunteer services.</p>
 
 		<h2 class= "title">Internships</h2>
 			<figure class="img3">
@@ -51,10 +51,10 @@
 			</figure>
 
 			<p>
-				<span>I</span>n 2016, I interned with the Committee Against Anti-Asian Violence’s Asian Youth in Action program. As an intern, I worked with the Chinatown Tenants Union to help tenants challenge evictions and harassment from their landlords. I also interpreted for rent stabilized tenants sharing their stories at hearings to fight for a rent freeze. Working at this organization allowed me to realize that striving for a collective goal is not only necessary, but can also make housing justice for low income residents a reality.</p>
+				In 2016, I interned with the Committee Against Anti-Asian Violence’s Asian Youth in Action program. As an intern, I worked with the Chinatown Tenants Union to help tenants challenge evictions and harassment from their landlords. I also interpreted for rent stabilized tenants sharing their stories at hearings to fight for a rent freeze. Working at this organization allowed me to realize that striving for a collective goal is not only necessary, but can also make housing justice for low income residents a reality.</p>
 
 			<p>
-				<span>F</span>or the past two years, I’ve been an intern at the American Museum of Natural History’s ornithology department. I conducted library and laboratory research on the genetic diversity of European starlings. The goal of the project was to determine if there were any distinct genetic differences between the European starlings of North America and their parent population. The research included extracting DNA from the flesh of the bird specimens and amplifying mitochondrial DNA from the birds. In addition, I used a bioinformatics software called Geneious to analyze and edit the DNA sequences. The program culminated in a research presentation at a symposium session.</p>
+				For the past two years, I’ve been an intern at the American Museum of Natural History’s ornithology department. I conducted library and laboratory research on the genetic diversity of European starlings. The goal of the project was to determine if there were any distinct genetic differences between the European starlings of North America and their parent population. The research included extracting DNA from the flesh of the bird specimens and amplifying mitochondrial DNA from the birds. In addition, I used a bioinformatics software called Geneious to analyze and edit the DNA sequences. The program culminated in a research presentation at a symposium session.</p>
 
 		<h2 class = "title">Courses</h2>
 			<figure class="img2">
@@ -63,10 +63,10 @@
 			</figure>
 
 			<p>
-				<span>T</span>he easiest classes that I’ve taken here at the University of Rochester are CSC170, PSY101 and CHI102. I was able to deepen my knowledge of creating and designing web pages with CSC170. The concepts in PSY101 were not difficult to understand and the mini courses throughout the semester made the class more interesting. CHI102 was not a difficult course, but it required a lot of work and practice every day to understand the vocabulary and grammar concepts.</p>
+				The easiest classes that I’ve taken here at the University of Rochester are CSC170, PSY101 and CHI102. I was able to deepen my knowledge of creating and designing web pages with CSC170. The concepts in PSY101 were not difficult to understand and the mini courses throughout the semester made the class more interesting. CHI102 was not a difficult course, but it required a lot of work and practice every day to understand the vocabulary and grammar concepts.</p>
 
 			<p>
-				<span>T</span>he harder classes include BIO110 and CHM131. For me, BIO110 was more difficult because it focused mainly on the molecular level of biology. CHM131 was fine until I started to learn about acids and titrations. Another class that was intense was MTH162. Even though the AP exam gave me a good foundation, a lot of the topics were difficult to understand, such as series.</p>
+				The harder classes include BIO110 and CHM131. For me, BIO110 was more difficult because it focused mainly on the molecular level of biology. CHM131 was fine until I started to learn about acids and titrations. Another class that was intense was MTH162. Even though the AP exam gave me a good foundation, a lot of the topics were difficult to understand, such as series.</p>
 
 		</article>
 
