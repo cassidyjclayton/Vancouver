17dd951db786eaa8b10bb692bc79526a31f7c48d
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-26 01:02:57 -0400

    removed <span> from index

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 
 
 		<p>
-			<span>M</span>y name is Lingling Li and my English name is Lily. I come from Shanghai, China. I love playing the piano, drawing, cooking and playing the badminton. I’m now a sophomore, and also the D’Lion in charge of Gate 6 and Gannett 6. My job
+			My name is Lingling Li and my English name is Lily. I come from Shanghai, China. I love playing the piano, drawing, cooking and playing the badminton. I’m now a sophomore, and also the D’Lion in charge of Gate 6 and Gannett 6. My job
 			is to help the first year student better move in to the brand new college life, and be their first friend in the college. I’m planning to have a double major in Business and Digital Media Study, minor in Japanese. A fun fact of me: I can never
 			counts how many steps I had done when doing the “Running jump shot”
 		</p>
@@ -52,7 +52,7 @@
 		</figure>
 
 		<p>
-			<span>I</span>’m a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events. I was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys.
+		I’m a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events. I was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys.
 		</p>
 
 		<h2 class="hometitle">Kevin | Brief Introduction</h2>
@@ -65,7 +65,7 @@
 		</figure>
 
 		<p>
-			<span>H</span>ello there! My name is Kevin Kao. I was born in New York but went to Taiwan for primary and secondary educations! I am currently a student (sophomore) doubling major in Computer Science and Mathematics at the University of Rochester. I have
+		Hello there! My name is Kevin Kao. I was born in New York but went to Taiwan for primary and secondary educations! I am currently a student (sophomore) doubling major in Computer Science and Mathematics at the University of Rochester. I have
 			strong interest in the field of science, and I have taken many courses related to my major last year. For example, I took CSC (170, 171, 172) and MTH (150, 165, 171, 172). In addition to that, I am now taking CSC (173, 174, 214) and MTH (173,
 			190, 201) to better prepare me for my career. Here I would like to share what triggers me to become a copmuter scientist and why I love technology so much!
 		</p>
