65d0282e4431b60605af3fc0c352a68d0fb8e92c
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-26 23:39:36 -0400

    rearranged valerie's content
    Val's stuff is finished until she sends me some of her goals, and which point I will edit that in in paragraph or div form, depending on how much info she gives me.

--- valerie.php
+++ valerie.php
@@ -23,45 +23,63 @@
 				<figcaption>Here's a picture of Valerie</figcaption>
 			</figure>
 
-		<h2>About Me</h2>
+		<h2 class= "title">About Me</h2>
 			
 
 			<p>
-				Im a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events.</p>
+				I'm a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I love food, and love trying new restaurants and cafes!
+			</p>
 
-			<p>
-				I was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys.</p>
-
-		<h2>Early Life and Education</h2>
 			
 
-			<p>
-				My first job was at Mini Ivy Tutoring Center, where I tutored elementary and middle school students in math and English. In addition, I performed various office administrative duties, such as keeping track of attendance and picking up phone calls.</p>
+		<h2 class= "title">Early Life and Education</h2>
 
 			<p>
-				In the summer of 2015, I participated in Girls Who Code’s Summer Immersion Program, where I was introduced to computer science. I learned how to code in JavaScript, Python, and HTML. Besides gaining technical skills, I was able to connect with female engineers and entrepreneurs, and hear their stories about what it’s like to work in a tech company. The program also gave me the opportunity to visit different tech companies, such as Google and Microsoft. For the final project, I created a user-friendly online app using HTML and JavaScript to locate and post about volunteer services.</p>
+				I was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys.
+			</p>
+			<p>
+				The easiest classes that I’ve taken here at the University of Rochester are CSC170, PSY101 and CHI102. I was able to deepen my knowledge of creating and designing web pages with CSC170. The concepts in PSY101 were not difficult to understand and the mini courses throughout the semester made the class more interesting. CHI102 was not a difficult course, but it required a lot of work and practice every day to understand the vocabulary and grammar concepts.
+			</p>
+
+			<p>
+				The harder classes include BIO110 and CHM131. For me, BIO110 was more difficult because it focused mainly on the molecular level of biology. CHM131 was fine until I started to learn about acids and titrations. Another class that was intense was MTH162. Even though the AP exam gave me a good foundation, a lot of the topics were difficult to understand, such as series.
+			</p>
+			
+
+			
 
 		<h2 class= "title">Experience</h2>
 			
 			<p>
-				In 2016, I interned with the Committee Against Anti-Asian Violence’s Asian Youth in Action program. As an intern, I worked with the Chinatown Tenants Union to help tenants challenge evictions and harassment from their landlords. I also interpreted for rent stabilized tenants sharing their stories at hearings to fight for a rent freeze. Working at this organization allowed me to realize that striving for a collective goal is not only necessary, but can also make housing justice for low income residents a reality.</p>
+				My first job was at Mini Ivy Tutoring Center, where I tutored elementary and middle school students in math and English. In addition, I performed various office administrative duties, such as keeping track of attendance and picking up phone calls. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events.
+			</p>
 
 			<p>
-				For the past two years, I’ve been an intern at the American Museum of Natural History’s ornithology department. I conducted library and laboratory research on the genetic diversity of European starlings. The goal of the project was to determine if there were any distinct genetic differences between the European starlings of North America and their parent population. The research included extracting DNA from the flesh of the bird specimens and amplifying mitochondrial DNA from the birds. In addition, I used a bioinformatics software called Geneious to analyze and edit the DNA sequences. The program culminated in a research presentation at a symposium session.</p>
-
-		<h2>Goals</h2>
-			
-			<p>
-				The easiest classes that I’ve taken here at the University of Rochester are CSC170, PSY101 and CHI102. I was able to deepen my knowledge of creating and designing web pages with CSC170. The concepts in PSY101 were not difficult to understand and the mini courses throughout the semester made the class more interesting. CHI102 was not a difficult course, but it required a lot of work and practice every day to understand the vocabulary and grammar concepts.</p>
+				In the summer of 2015, I participated in Girls Who Code’s Summer Immersion Program, where I was introduced to computer science. I learned how to code in JavaScript, Python, and HTML. Besides gaining technical skills, I was able to connect with female engineers and entrepreneurs, and hear their stories about what it’s like to work in a tech company. The program also gave me the opportunity to visit different tech companies, such as Google and Microsoft. For the final project, I created a user-friendly online app using HTML and JavaScript to locate and post about volunteer services.
+			</p>
 
 			<p>
-				The harder classes include BIO110 and CHM131. For me, BIO110 was more difficult because it focused mainly on the molecular level of biology. CHM131 was fine until I started to learn about acids and titrations. Another class that was intense was MTH162. Even though the AP exam gave me a good foundation, a lot of the topics were difficult to understand, such as series.</p>
+				In 2016, I interned with the Committee Against Anti-Asian Violence’s Asian Youth in Action program. As an intern, I worked with the Chinatown Tenants Union to help tenants challenge evictions and harassment from their landlords. I also interpreted for rent stabilized tenants sharing their stories at hearings to fight for a rent freeze. Working at this organization allowed me to realize that striving for a collective goal is not only necessary, but can also make housing justice for low income residents a reality.
+			</p>
+
+			<p>
+				For the past two years, I’ve been an intern at the American Museum of Natural History’s ornithology department. I conducted library and laboratory research on the genetic diversity of European starlings. The goal of the project was to determine if there were any distinct genetic differences between the European starlings of North America and their parent population. The research included extracting DNA from the flesh of the bird specimens and amplifying mitochondrial DNA from the birds. In addition, I used a bioinformatics software called Geneious to analyze and edit the DNA sequences. The program culminated in a research presentation at a symposium session.
+			</p>
+
+		<h2 class= "title">Goals</h2>
+		
 
 		</main>
 
 		<footer>
+			<h2 class= "title">
+				Source
+			</h2>
+			<div>
+				<a href="http://csc174.org/assignment01/vtam/index.html">Initial Personal Webpage | Valerie</a>
+			</div>
 			<div class="bottom">
-			This webpage, by team Vancouver. Original Source <a href="http://csc174.org/assignment01/vtam/index.html">Initial Personal Webpage | Valerie</a>
+			This webpage, by team Vancouver. 
 			</div>
 		</footer>
 
