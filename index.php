214990eb5718d17d930019179ec436966628ca18
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-27 00:08:44 -0400

    corrected index formatting; added extra nav links for choreo purposes

--- index.php
+++ index.php
@@ -14,32 +14,37 @@
 <body>
 
 		<header>
-			<h1>Technically, We're All Connected: A Profile of Three STEAM Students</h1>
+			<h1>
+				Technically, We're All Connected: A Profile of Three STEAM Students
+			</h1>
 		</header>
 
 		<?php include "inc/nav.inc"; ?>
 
-	<article>
+	<main>
 
 		<img class="img1" src="img/line.jpg" alt="section line">
 
 
-		<h2 class="hometitle"> Lingling | Brief Introduction</h2>
+		<h2 class="hometitle"> 
+			Lingling | Brief Introduction
+		</h2>
 
 		<figure class="img2">
 			<img src="img/lingling-headshot.jpg" alt="the picture of Lingling">
-
 			<figcaption class="figcaption1">
 				Here is a picture of Lingling
 			</figcaption>
 		</figure>
 
 
-		<p>
-			My name is Lingling Li, and my English name is Lily. I come from Shanghai, China. I love playing the piano, drawing, cooking and playing badminton. I’m now a sophomore, and also the D’Lion in charge of Gate 6 and Gannett 6. My job is to help the first year students better adjust to their brand-new college life, and to be their first friend on campus. I’m planning to double major in Business and Digital Media Studies, and to minor in Japanese. <a href="lingling.php"> Read More...</a>
-		</p>
+			<p>
+				My name is Lingling Li, and my English name is Lily. I come from Shanghai, China. I love playing the piano, drawing, cooking and playing badminton. I’m now a sophomore, and also the D’Lion in charge of Gate 6 and Gannett 6. My job is to help the first year students better adjust to their brand-new college life, and to be their first friend on campus. I’m planning to double major in Business and Digital Media Studies, and to minor in Japanese. <a href="lingling.php"> Read More...</a>
+			</p>
 
-		<h2 class="hometitle"> Valerie | Brief Introduction</h2>
+		<h2 class="hometitle">
+			Valerie | Brief Introduction
+		</h2>
 
 		<figure class="img3">
 			<img src="img/valerie-headshot.jpg" alt="Valerie Tam">
@@ -48,11 +53,14 @@
 			</figcaption>
 		</figure>
 
-		<p>
-		I’m a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events. I was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys.
-		</p>
+			<p>
+				I’m a sophomore at the University of Rochester majoring in data science with clusters in Chinese and Psychology. I am also the marketing director of the Hong Kong Students’ Association. As the marketing director, I update the club’s social media pages and create posters for events. I was born and raised in New York City. Some of my favorite things to do in New York City include walking around Soho and going to the best bubble tea shops, such as Bar Pa Tea and Yaya Tea. In addition, I consider myself a foodie, always trying the newest restaurant or dessert shop which is why I created a foodstagram to document all of my food journeys. <a href="valerie.php"> Read More...</a>
+			</p>
 
-		<h2 class="hometitle">Kevin | Brief Introduction</h2>
+
+		<h2 class="hometitle">
+			Kevin | Brief Introduction
+		</h2>
 
 		<figure class="img2" >
 			<img src="img/kevin.jpg" alt="Kevin Kao">
@@ -61,16 +69,15 @@
 			</figcaption>
 		</figure>
 
-		<p>
-		Hello there! My name is Kevin Kao. I was born in New York but went to Taiwan for primary and secondary educations! I am currently a student (sophomore) doubling major in Computer Science and Mathematics at the University of Rochester. I have
-			strong interest in the field of science, and I have taken many courses related to my major last year. For example, I took  <em>CSC (170, 171, 172)</em> and <em>MTH (150, 165, 171, 172)</em>. In addition to that, I am now taking <em>CSC (173, 174, 214)</em> and <em>MTH (173, 190, 201)</em> to better prepare me for my career. Here I would like to share what triggers me to become a copmuter scientist and why I love technology so much!
-		</p>
+			<p>
+				Hello there! My name is Kevin Kao. I was born in New York but went to Taiwan for primary and secondary educations! I am currently a student (sophomore) doubling major in Computer Science and Mathematics at the University of Rochester. I have a strong interest in the field of science, and I have taken many courses related to my major last year. For example, I took <em>CSC (170, 171, 172)</em> and <em>MTH (150, 165, 171, 172)</em>. In addition to that, I am now taking <em>CSC (173, 174, 214)</em> and <em>MTH (173, 190, 201)</em> to better prepare me for my career. Here I would like to share what triggers me to become a copmuter scientist and why I love technology so much! <a href="kevin.php"> Read More...</a>
+			</p>
 
-	</article>
+	</main>
 
 	 <footer>
 	 	<div class="bottom">
-	 		This webpage by team Vancouver
+	 		This webpage by Team Vancouver
 	 	</div>
 	 </footer>
 
