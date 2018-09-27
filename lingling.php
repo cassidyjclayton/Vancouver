938a93f71e0c745f244302dcbec1d4dc48a08686
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-26 23:47:01 -0400

    added title class to lingling's h2s

--- lingling.php
+++ lingling.php
@@ -33,41 +33,55 @@
 			</figcaption>
 		</figure>
 
-		<h2>About Me</h2>
-		<p>
-			My name is Lingling Li, and my English name is Lily. I come from Shanghai, China. I love playing the piano, drawing, cooking and playing badminton. I’m now a sophomore, and also the D’Lion in charge of Gates 6 and Gannett 6. My job is to help the first year students better adjust to college life, and to be their first friend in college. I’m planning to double major in Business and Digital Media Studies, and minor in Japanese.
-		</p>
-
-		<h2>Early Life and Education</h2>
-		<p>
-			I come from Shanghai, China, which is a modern and beautiful city. The place I miss the most is the park near my kindergarten. By the age of 4, most of my peers were attending kindergarten, and starting their "academic year". I, on the other hand, was still playing--or in other words, "studying" at home. My parents think that family education is better for a young kid than attending class in the kindergarten, although "family education" is not as popular in China as in America. I thank my parents a lot for letting me have that experience.
-		</p>
-
-		<p>
-			I have taken almost all pre-requesite courses for my major. Of all the classes I've taken, JPN101 is my favorite class. I tried to keep my course load within 24 credits, but thereare simply too many interesting subjects I want to explre while in college, so I overloaded again this semester. I hope I can succeed in this year’s academic study.	I love to play the piano, and am currently taking lessons at Eastman music school. I started to learn piano (as well as my painting) when I was in Kindergarten. Yes! Kindergarten again! But it was not until I attended piano lessons at Eastman that I really started to love playing the piano. The teacher in Eastman simply overturned everything I thought I knew about the piano and how to practice the piano. "You must think the piano as an assembled collection of instruments,” my teacher said. I started to explore the unique tone quality of different region on the keyboard, and that makes me really excited. Just imagine! You can play as a band while you are just actually playing the single instrument!
-		</p>
-
-		<p>
-			Besides the piano, I'm also trying to explore other possibilities! For sports, I love play badminton, and successfully managed to realize my previous semester's goal to play 4 hours of sports every week. This semester, I want to explore the dancing area! 
-		</p>
-
-		<h2> Experience </h2>
-		
+		<h2 class="title">
+		About Me
+		</h2>
 			<p>
-				I love painting and designing, and had an internship this summer as a graphic designer in a Fin-tech company. I registered for the "Rice Crew" club to have a taste of K-pop dancing ths semester. I'm also trying to do something for the community. I passed the elections to be a D'Lion this year on Gate 6 and Gannett 6, aiming to help the first-year students move into the campus and acted as a student leader on behalf on the "social sprit”.
+			My name is Lingling Li, and my English name is Lily. I come from Shanghai, China. I love playing the piano, drawing, cooking and playing badminton. I’m now a sophomore, and also the D’Lion in charge of Gates 6 and Gannett 6. My job is to help the first year students better adjust to college life, and to be their first friend in college. I’m planning to double major in Business and Digital Media Studies, and minor in Japanese.
 			</p>
 
-		<h2> Goals</h2>
+		<h2 class="title">
+			Early Life and Education
+		</h2>
+			<p>
+			I come from Shanghai, China, which is a modern and beautiful city. The place I miss the most is the park near my kindergarten. By the age of 4, most of my peers were attending kindergarten, and starting their "academic year". I, on the other hand, was still playing--or in other words, "studying" at home. My parents think that family education is better for a young kid than attending class in the kindergarten, although "family education" is not as popular in China as in America. I thank my parents a lot for letting me have that experience.
+			</p>
+
+			<p>
+			I have taken almost all pre-requesite courses for my major. Of all the classes I've taken, JPN101 is my favorite class. I tried to keep my course load within 24 credits, but thereare simply too many interesting subjects I want to explre while in college, so I overloaded again this semester. I hope I can succeed in this year’s academic study.	I love to play the piano, and am currently taking lessons at Eastman music school. I started to learn piano (as well as my painting) when I was in Kindergarten. Yes! Kindergarten again! But it was not until I attended piano lessons at Eastman that I really started to love playing the piano. The teacher in Eastman simply overturned everything I thought I knew about the piano and how to practice the piano. "You must think the piano as an assembled collection of instruments,” my teacher said. I started to explore the unique tone quality of different region on the keyboard, and that makes me really excited. Just imagine! You can play as a band while you are just actually playing the single instrument!
+			</p>
+
+			<p>
+			Besides the piano, I'm also trying to explore other possibilities! For sports, I love play badminton, and successfully managed to realize my previous semester's goal to play 4 hours of sports every week. This semester, I want to explore the dancing area! 
+			</p>
+
+		<h2 class="title">
+		 Experience 
+		</h2>
 		
-		<p>
-				For my academic career: I might return to China and work for some years. After I gain some savings and accumulate some life experience, I might return to school again. I might go to more countries to finish my graduate education. I plan to stay in Australia, England, German to have my graduate education. I believe the time being a student is the happiest time in life--you can enjoy the world, enjoy the travel and enjoy the knowledge. After graduate school, I might return to work for several years, and I' like to one day maybe even start my own business.
+			<p>
+			I love painting and designing, and had an internship this summer as a graphic designer in a Fin-tech company. I registered for the "Rice Crew" club to have a taste of K-pop dancing ths semester. I'm also trying to do something for the community. I passed the elections to be a D'Lion this year on Gate 6 and Gannett 6, aiming to help the first-year students move into the campus and acted as a student leader on behalf on the "social sprit”.
+			</p>
+
+		<h2 class="title">
+		 Goals
+		</h2>
+		
+			<p>
+			For my academic career: I might return to China and work for some years. After I gain some savings and accumulate some life experience, I might return to school again. I might go to more countries to finish my graduate education. I plan to stay in Australia, England, German to have my graduate education. I believe the time being a student is the happiest time in life--you can enjoy the world, enjoy the travel and enjoy the knowledge. After graduate school, I might return to work for several years, and I' like to one day maybe even start my own business.
 			</p>
 
 	</main>
 
 	<footer>
+		<h2 class="title">
+			Source
+		</h2>
+		<div>
+			<a href="http://www.csc174.org/assignment01/lli53/index.php">Initial Personal Webpage | Lingling</a>
+		</div>
 		<div class="bottom">
-			This webpage, by team Vancouver. Original Source <a href="http://www.csc174.org/assignment01/lli53/index.php">Initial Personal Webpage | Lingling</a>
+			This webpage, by Team Vancouver. 
 		</div>
 	</footer>
  <?php include "inc/scripts.inc"; ?>
